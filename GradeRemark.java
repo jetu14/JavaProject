61419c4ab6861c0cda12b3b5561b10246bbe0c2e
Nayem IslamJetu <nayemislamjetu@gmail.com>
2025-07-12 00:03:43 +0600

    Add files via upload

---
+++ GradeRemark.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.graderemark;
+
+/**
+ *
+ * @author MSi
+ */
+public class GradeRemark {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
